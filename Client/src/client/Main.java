3f99646da8e05f495d81b345878c3f091a9017df
Nikita Dyatlov <nexodrone@gmail.com>
2016-04-22 10:38:50 -0300

    Modulos de servidor y cliente separados con implementacion nula.

---
+++ Client/src/client/Main.java
@@ -0,0 +1,7 @@
+package client;
+
+public class Main {
+    public static void main(String[] ar) {
+        System.out.println("Client");
+    }
+}
--- Client/test/MainTests.java
+++ Client/test/MainTests.java
@@ -1,5 +1,3 @@
-package ar.fiuba.tdd.tp;
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
