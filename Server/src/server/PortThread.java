bf79bc736850d5a1ca442721e9fea201e261e637
Nikita Dyatlov <nexodrone@gmail.com>
2016-04-22 21:00:30 -0300

    Extracting PortThread.

---
+++ Server/src/server/PortThread.java
@@ -0,0 +1,64 @@
+package server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class PortThread implements Runnable {
+
+    private ServerSocket ss;
+
+    InputStream inputStream = null;
+    DataInputStream dataInputStream = null;
+    OutputStream outputStream = null;
+    DataOutputStream dataOutputStream = null;
+
+    String sendByClient = "";
+
+    public PortThread(ServerSocket ss) {
+        this.ss = ss;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Ready and listening port " + ss.getLocalPort());
+            Socket socket = ss.accept();
+            System.out.println("Port " + ss.getLocalPort() + " got a client!");
+
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
+
+            dataInputStream = new DataInputStream(inputStream);
+            dataOutputStream = new DataOutputStream(outputStream);
+
+            goMonitoring();
+
+        } catch (IOException e) {
+            System.out.println("Unable to accept client!");
+        }
+    }
+
+    private void sendAnswer() {
+        try {
+            dataOutputStream.writeUTF("[DUMB ANSWER]");
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            System.out.println("Unable to send answer to client connected to port " + ss.getLocalPort());
+        }
+    }
+
+    private void goMonitoring() {
+        boolean isConnected = true;
+        while (isConnected) {
+            try {
+                sendByClient = dataInputStream.readUTF();
+                System.out.println("Port " + ss.getLocalPort() + " got a message: " + sendByClient);
+                sendAnswer();
+            } catch (IOException e) {
+                System.out.println("Port " + ss.getLocalPort() + " has disconnected");
+                isConnected = false;
+            }
+        }
+    }
+}
