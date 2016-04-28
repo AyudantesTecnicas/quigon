de73ac24edb04ffd2e41440862e898ef92f76cea
meister4ever <sasavulsky@gmail.com>
2016-04-28 14:52:04 -0300

    Game Correction: old Tests deleted

--- Server/test/creationalTests.java
+++ Server/test/creationalTests.java
@@ -17,18 +17,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class creationalTests {
-    @Test
-    public void amountOfRoomsIsCorrect(){
-        GameCreator gameCreator = new GameCreator();
-        gameCreator.createGame("OpenDoor");
-        assertEquals(gameCreator.getGame().getRooms().size(), 2);
-    }
-    @Test
-    public void amountOfItemsIsCorrect(){
-        GameCreator gameCreator = new GameCreator();
-        gameCreator.createGame("OpenDoor");
-        assertEquals(gameCreator.getGame().getRooms().get(0).itemsSize(), 2);
-    }
+
     @Test
     public void getNameCorrect(){
         GameCreator gameCreator = new GameCreator();
