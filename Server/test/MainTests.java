4098632cb0165afaa5351882b89080883932a680
Nikita Dyatlov <nexodrone@gmail.com>
2016-04-28 14:02:05 -0300

    Maldito MainTests

--- Server/test/MainTests.java
+++ Server/test/MainTests.java
@@ -1,10 +1,11 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MainTests {
 
     @Test
-    void dummy() {
-        assert (true);
+    public void dummy() {
+        assertEquals(0, 0);
     }
-
 }
