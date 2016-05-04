c2e4889f8e1ad3f3d096fd631cbc0c906da33d0a
AgustinBinci <agustinbinci@gmail.com>
2016-05-03 23:15:28 -0300

    Unifico en "ComplexElement" la logica de contener elementos que antes tenia "ComplexContainerElement"

--- Server/src/Model/elements/ComplexElement.java
+++ Server/src/Model/elements/ComplexElement.java
@@ -11,9 +11,10 @@ public class ComplexElement extends Element implements Comparable<ComplexElement
     //Attributes
     private List<Element> states;
     private List<Move> moves;
-    private Element containerElement;
+    private ComplexElement containerElement;
     private Integer size;
     private Boolean visible;
+    private List<ComplexElement> elements;
 
     //Methods
     public ComplexElement() {
@@ -48,8 +49,10 @@ public class ComplexElement extends Element implements Comparable<ComplexElement
         }
     }
 
-    public void setContainerElement(Element containerElement) {
+    public void setContainerElement(ComplexElement containerElement) {
+        this.containerElement.removeElement(this);
         this.containerElement = containerElement;
+        containerElement.addElement(this);
     }
 
     public Element getContainerElement() {
@@ -86,11 +89,12 @@ public class ComplexElement extends Element implements Comparable<ComplexElement
         this.setContainerElement(null);
         this.size = 0;
         this.visible = true;
+        this.elements = null;
     }
 
     public String execute(String moveName) {
         Iterator<Move> iterator = this.moves.iterator();
-        Move move = null;
+        Move move;
         String oneString = "Invalid Action";
         while (iterator.hasNext()) {
             move = iterator.next();
@@ -106,5 +110,48 @@ public class ComplexElement extends Element implements Comparable<ComplexElement
     @Override
     public int compareTo(ComplexElement other) {
         return (this.size - other.getSize());
+    }
+
+    public void addElement(ComplexElement element) {
+        if (element != null) {
+            if (this.elements == null) {
+                this.elements = new ArrayList<>();
+            }
+            if (!this.elements.contains(element)) {
+                this.elements.add(element);
+            }
+        }
+    }
+
+    public void removeElement(ComplexElement element) {
+        this.elements.remove(element);
+    }
+
+    public Boolean hasElement(ComplexElement element) {
+        return this.elements.contains(element);
+    }
+
+    public ComplexElement getElementAt(String index) {
+        Integer intIndex;
+
+        if (!index.isEmpty()) {
+            index = index.toLowerCase();
+
+            if (index.equals("last")) {
+                return this.elements.get(this.elements.size() - 1);
+            }
+
+            if (index.equals("first")) {
+                return this.elements.get(0);
+            }
+
+            intIndex = Integer.parseInt(index);
+
+            if (intIndex >= 0 && intIndex < this.elements.size()) {
+                return this.elements.get(intIndex);
+            }
+        }
+
+        return null;
     }
 }
