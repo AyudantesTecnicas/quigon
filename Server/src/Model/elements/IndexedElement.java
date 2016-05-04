c71ca1d3237fe8ea5d539f55266010b8c367a929
AgustinBinci <agustinbinci@gmail.com>
2016-05-03 23:16:21 -0300

    Creo una clase que contena un elemento y un indice para acceder (o no ) a un elemento que contenga el elemento contenedor

---
+++ Server/src/Model/elements/IndexedElement.java
@@ -0,0 +1,29 @@
+package Model.elements;
+
+public class IndexedElement {
+
+    //Attributes
+    private ComplexElement element;
+    private String index;
+
+    //Methods
+    public IndexedElement(ComplexElement element) {
+        this.element = element;
+        this.index = "";
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public ComplexElement getElement() {
+        //If does not have an index the element to update is the element itself
+        if (this.index.isEmpty()) {
+            return this.element;
+        }
+
+        //Else the element to update is an element of the element
+        return this.element.getElementAt(this.index);
+    }
+
+}
