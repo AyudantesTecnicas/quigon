3ae45a831dc0c64cb58e5b340e3779820def679b
meister4ever <sasavulsky@gmail.com>
2016-05-05 12:06:40 -0300

    DoesntHaveContainerRule

--- Server/src/Model/rules/DoesNotHaveContainerRule.java
+++ Server/src/Model/rules/DoesNotHaveContainerRule.java
@@ -2,6 +2,7 @@ package Model.rules;
 
 public class DoesNotHaveContainerRule extends RuleExpression {
 
+
     @Override
     public Boolean validate() {
         return !this.elementToValidate.hasContainerElement(this.elementOfElementToValidate);
