5cacd3fd8318b8b04e7e3d7e4d1c76c1a4d8f81a
AgustinBinci <agustinbinci@gmail.com>
2016-05-15 21:57:31 -0300

    Agrego las torres a una room principal y unica

--- src/main/java/games/constants/TowerOfHanoiConstants.java
+++ src/main/java/games/constants/TowerOfHanoiConstants.java
@@ -3,26 +3,32 @@ package games.constants;
 @SuppressWarnings("CPD-START")
 public class TowerOfHanoiConstants {
 
+    //Room
+    public static final String room = "room";
+
+    //Character
+    public static final String character = "character";
+
     //Index
     public static final String last = "last";
 
     //Stacks
-    public static final String stack1 = "Stack 1";
-    public static final String stack2 = "Stack 2";
-    public static final String stack3 = "Stack 3";
+    public static final String stack1 = "Stack1";
+    public static final String stack2 = "Stack2";
+    public static final String stack3 = "Stack3";
 
     //Discs
-    public static final String disc1 = "Disc 1";
-    public static final String disc2 = "Disc 2";
-    public static final String disc3 = "Disc 3";
-    public static final String disc4 = "Disc 4";
-    public static final String disc5 = "Disc 5";
-    public static final String disc6 = "Disc 6";
+    public static final String disc1 = "Disc1";
+    public static final String disc2 = "Disc2";
+    public static final String disc3 = "Disc3";
+    public static final String disc4 = "Disc4";
+    public static final String disc5 = "Disc5";
+    public static final String disc6 = "Disc6";
 
     //Moves
-    public static final String moveStack1 = "Move top stack 1";
-    public static final String moveStack2 = "Move top stack 2";
-    public static final String moveStack3 = "Move top stack 3";
+    public static final String moveStack1 = "Move1";
+    public static final String moveStack2 = "Move2";
+    public static final String moveStack3 = "Move3";
 
     //Messages
     public static final String moved = "Moved!";
