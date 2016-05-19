cd3ef8d5c898d821defeeb570ce7d7d3bfe7be2b
luciaCP <lt_caponpaul@hotmail.com>
2016-05-19 19:28:16 -0300

    Entrega ejecicio clase

--- EntregaBuilder/src/main/java/EntregaBuilder.java
+++ EntregaBuilder/src/main/java/EntregaBuilder.java
@@ -1,6 +1,29 @@
 import creation.GameBuilderImp;
+import model.elements.ComplexElement;
 
 public final class EntregaBuilder {
+
+    //Character
+    private ComplexElement character;
+
+    //Rooms
+    private ComplexElement roomPrincipal;
+    private ComplexElement roomWest;
+    private ComplexElement roomEast;
+    private ComplexElement roomSouth;
+    private ComplexElement roomNorth;
+
+    //Doors
+    private ComplexElement doorWtoC;
+    private ComplexElement doorEtoC;
+    private ComplexElement doorStoC;
+    private ComplexElement doorNtoC;
+    private ComplexElement doorCtoW;
+    private ComplexElement doorCtoE;
+    private ComplexElement doorCtoS;
+    private ComplexElement doorCtoN;
+
+    //Items
 
 }
 
---
+++ EntregaBuilder/src/main/java/EntregaConstants.java
@@ -0,0 +1,17 @@
+@SuppressWarnings("CPD-START")
+public class EntregaConstants {
+    //Elements
+    public static final String character = "character";
+    public static final String room = "room";
+    public static final String stick = "stick";
+
+    //Actions
+    public static final String pick = "pick";
+
+    //Rules
+    public static final String noStick = "There's no stick in room";
+    public static final String notWon = "it's a pity";
+
+    //Moves
+    public static final String pickStick = "You won the game!";
+}
