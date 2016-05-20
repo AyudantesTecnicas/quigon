ecd9eeb1f13c2795a71d54195e93254c35e5224a
luciaCP <lt_caponpaul@hotmail.com>
2016-05-20 08:59:36 -0300

    Push roto para compartir lo hecho en la entrega

--- EntregaBuilder/src/main/java/EntregaBuilder.java
+++ EntregaBuilder/src/main/java/EntregaBuilder.java
@@ -1,32 +1,297 @@
 import creation.GameBuilderImp;
+import logic.LogicBuilder;
+import model.actions.Action;
+import model.actions.Move;
 import model.elements.ComplexElement;
+import model.rulesexpressions.rules.*;
 
-public final class EntregaBuilder {
+public final class EntregaBuilder extends GameBuilderImp {
+
+    @SuppressWarnings("CPD-START")
+
+    public EntregaBuilder() {
+        gameName = "EntregaBuilder";
+        gameDescription = "EjercicioEntrega";
+    }
 
     //Character
     private ComplexElement character;
 
     //Rooms
-    private ComplexElement roomPrincipal;
-    private ComplexElement roomWest;
-    private ComplexElement roomEast;
-    private ComplexElement roomSouth;
-    private ComplexElement roomNorth;
-
-    //Doors
-    private ComplexElement doorWtoC;
-    private ComplexElement doorEtoC;
-    private ComplexElement doorStoC;
-    private ComplexElement doorNtoC;
-    private ComplexElement doorCtoW;
-    private ComplexElement doorCtoE;
-    private ComplexElement doorCtoS;
-    private ComplexElement doorCtoN;
+    private ComplexElement roomPasillo;
+    private ComplexElement roomSalon1;
+    private ComplexElement roomSalon2;
+    private ComplexElement roomSalon3;
+    private ComplexElement roomSotano;
+    private ComplexElement roomSubSotano;
+    private ComplexElement roomBiblioteca;
+    private ComplexElement roomAccesoBiblioteca;
+    private ComplexElement roomPatio;
 
     //Items
+    private ComplexElement itemFoto;
+    private ComplexElement itemLapicera;
+    //Items salon 1
+    private ComplexElement itemMesa;
+    private ComplexElement itemBotella;
+    private ComplexElement itemVaso1;
+    private ComplexElement itemVaso2;
+    private ComplexElement itemSilla1;
+    private ComplexElement itemSilla2;
+    private ComplexElement itemCuadroTren;
+    private ComplexElement itemCuadroBarco;
+    private ComplexElement itemCajaFuerte;
+    private ComplexElement itemCredencial;
+    //Items salon 2
+    private ComplexElement itemMartillo;
+    private ComplexElement itemDestornillador1;
+    private ComplexElement itemDestornillador2;
+    //Items salon 3
+    private ComplexElement itemLlave;
+    //Items Acceso Bilioteca
+    private ComplexElement itemBibliotecario;
 
+    //Rules
+    private LogicBuilder logicBuilder = new LogicBuilder();
+    private HasContainerRule ruleTenerLlave;
+    private HasStateRule ruleCredencialValida;
+
+    //Item actions
+    private Action actionPickKey;
+    private Action actionPickMartillo;
+    private Action actionPickDestornillador1;
+    private Action actionPickDestornillador2;
+    private Action actionPickBotella;
+    private Action actionPickVaso1;
+    private Action actionPickVaso2;
+    private Action actionPickCredencial;
+    private Action actionSetVisibleCajaFuerte;
+    private Action actionSetVisibleCredencial;
+    private Action actionChangeToPasillo;
+    private Action actionChangeToSalon1;
+    private Action actionChangeToSalon2;
+    private Action actionChangeToSalon3;
+    private Action actionChangeToAccesoBiblioteca;
+    private Action actionChangeToBiblioteca;
+    private Action actionPutFotoEnCredencial;
+    private Action actionSetCredencialToValida;
+    private Action actionSetCredencialToInvalida;
+    private Action actionMakeBibliotecarioFeliz;
+
+    //Moves
+    private Move moveMoverCuadro;
+    private Move moveAbrirCajaFuerte;
+    private Move moveTomarCredencial;
+    private Move moveIrAPasillo;
+    private Move moveIrASalon1;
+    private Move moveIrASalon2;
+    private Move moveIrASalon3;
+    private Move moveIrAAccesoBiblioteca;
+    private Move movePonerFotoEnCredencial;
+
+    //Doors
+    private ComplexElement doorPasilloToSalon1;
+    private ComplexElement doorPasilloToSalon2;
+    private ComplexElement doorPasilloToSalon3;
+    private ComplexElement doorPasilloToAccesoBiblioteca;
+    private ComplexElement doorSalon1ToPasillo;
+    private ComplexElement doorSalon2ToPasillo;
+    private ComplexElement doorSalon3ToPasillo;
+    private ComplexElement doorAccesoBibliotecaToPasillo;
+    private ComplexElement doorAccesoBibliotecaToBiblioteca;
+    private ComplexElement doorBibliotecaToAccesoBiblioteca;
+
+    //States
+    private ComplexElement stateValido;
+    private ComplexElement stateInvalido;
+    private ComplexElement stateOpen;
+    private ComplexElement stateFeliz;
+
+    protected void setActions() {}
+
+    public void setElements() {
+        createRooms();
+        defineCharacter();
+        createStates();
+        createDoors();
+        createItems();
+        createRoomTransitionActions();
+        createDoorActions();
+        createItemActions();
+        createRules();
+        defineVictoryRule();
+        createMoves();
+        addMoves();
+    }
+
+    private void createRoomTransitionActions() {}
+    private void createDoorActions() {}
+    private void defineVictoryRule() {}
+
+
+    private void defineCharacter() {
+        character = createAndAddElement(EntregaConstants.character, roomPasillo, null);
+        game.character = character;
+    }
+
+    private void createRooms() {
+        roomPasillo = createAndAddElement(EntregaConstants.roomPasillo, null, null);
+        roomSalon1 = createAndAddElement(EntregaConstants.roomSalon1, null, null);
+        roomSalon2 = createAndAddElement(EntregaConstants.roomSalon2, null, null);
+        roomSalon3 = createAndAddElement(EntregaConstants.roomSalon3, null, null);
+        roomSotano = createAndAddElement(EntregaConstants.roomSotano, null, null);
+        roomSubSotano = createAndAddElement(EntregaConstants.roomSubSotano, null, null);
+        roomBiblioteca = createAndAddElement(EntregaConstants.roomBiblioteca, null, null);
+        roomAccesoBiblioteca = createAndAddElement(EntregaConstants.roomAccesoBiblioteca, null, null);
+        roomPatio = createAndAddElement(EntregaConstants.roomPatio, null, null);
+    }
+
+    private void createStates() {
+        stateOpen = new ComplexElement(EntregaConstants.abierta);
+        stateInvalido = new ComplexElement(EntregaConstants.invalido);
+        stateValido = new ComplexElement(EntregaConstants.valido);
+        stateFeliz = new ComplexElement(EntregaConstants.feliz);
+    }
+
+    private void createDoors() {
+        //Doors from Pasillo to another one
+        doorPasilloToSalon1 = createAndAddElement(EntregaConstants.doorPasilloToSalon1, roomPasillo, stateOpen);
+        doorPasilloToSalon2 = createAndAddElement(EntregaConstants.doorPasilloToSalon2, roomPasillo, stateOpen);
+        doorPasilloToSalon3 = createAndAddElement(EntregaConstants.doorPasilloToSalon3, roomPasillo, stateOpen);
+        doorPasilloToAccesoBiblioteca = createAndAddElement(EntregaConstants.doorAccesoBiblioteca, roomPasillo, stateOpen);
+
+        //Doors to Pasillo from another one
+        doorSalon1ToPasillo = createAndAddElement(EntregaConstants.doorPasillo, roomSalon1, stateOpen);
+        doorSalon2ToPasillo = createAndAddElement(EntregaConstants.doorPasillo, roomSalon2, stateOpen);
+        doorSalon3ToPasillo = createAndAddElement(EntregaConstants.doorPasillo, roomSalon3, stateOpen);
+        doorAccesoBibliotecaToPasillo = createAndAddElement(EntregaConstants.doorPasillo, roomAccesoBiblioteca, stateOpen);
+
+        //Others doors
+        doorAccesoBibliotecaToBiblioteca = createAndAddElement(EntregaConstants.doorBiblioteca, roomAccesoBiblioteca, stateOpen);
+        doorBibliotecaToAccesoBiblioteca = createAndAddElement(EntregaConstants.doorAccesoBiblioteca, roomBiblioteca, stateOpen);
+    }
+
+    private void createPickItemsAction() {
+        actionPickKey = buildChangeContainerAction(itemLlave, character);
+        actionPickMartillo = buildChangeContainerAction(itemMartillo, character);
+        actionPickDestornillador1 = buildChangeContainerAction(itemDestornillador1, character);
+        actionPickDestornillador2 = buildChangeContainerAction(itemDestornillador2, character);
+        actionPickBotella = buildChangeContainerAction(itemBotella, character);
+        actionPickVaso1 = buildChangeContainerAction(itemVaso1, character);
+        actionPickVaso2 = buildChangeContainerAction(itemVaso2, character);
+        actionPickCredencial = buildChangeContainerAction(itemCredencial, character);
+    }
+
+    private void createChangeRoomItemsAction() {
+        actionChangeToPasillo = buildChangeContainerAction(character, roomPasillo);
+        actionChangeToSalon1 = buildChangeContainerAction(character,roomSalon1);
+        actionChangeToSalon2 = buildChangeContainerAction(character,roomSalon2);
+        actionChangeToSalon3 = buildChangeContainerAction(character,roomSalon3);
+        actionChangeToAccesoBiblioteca = buildChangeContainerAction(character, roomAccesoBiblioteca);
+        actionChangeToBiblioteca = buildChangeContainerAction(character, roomBiblioteca);
+    }
+
+    private void createItemActions() {
+        createPickItemsAction();
+        createChangeRoomItemsAction();
+
+        actionSetVisibleCajaFuerte = buildChangeContainerAction(itemCajaFuerte,roomSalon1);
+        actionSetVisibleCredencial = buildChangeContainerAction(itemCredencial,roomSalon1);
+
+        actionPutFotoEnCredencial = buildChangeContainerAction(itemFoto, itemCredencial);
+        actionSetCredencialToValida = buildAddStatesAction(itemCredencial, stateValido);
+        actionSetCredencialToInvalida = buildAddStatesAction(itemCredencial, stateInvalido);
+
+        // TODO: Agregar makeBibliotecarioBorracho
+        // Con el anterior y el actionMakeBibliotecarioFeliz se va a validar si se puede pasar a biblioteca
+        actionMakeBibliotecarioFeliz = buildAddStatesAction(itemBibliotecario, stateFeliz);
+        actionMakeBibliotecarioFeliz.setRules(ruleCredencialValida);
+    }
+
+    private void createRules() {
+        ruleTenerLlave = checkContainerRule(itemLlave,character,EntregaConstants.necesitaTenerLlaveSalon3);
+        ruleCredencialValida = checkStateRule(itemCredencial, stateValido, EntregaConstants.necesitaSerValida);
+    }
+
+    private void createMoves() {
+        moveMoverCuadro = moveWithActionsAndRules(EntregaConstants.moveCuadro, actionSetVisibleCajaFuerte,
+                null, EntregaConstants.movedCuadroBarco);
+        moveAbrirCajaFuerte = moveWithActionsAndRules(EntregaConstants.moveAbrirCajaFuerte, actionSetVisibleCredencial,
+                ruleTenerLlave, EntregaConstants.abiertaCajaFuerte);
+        moveTomarCredencial = moveWithActionsAndRules(EntregaConstants.tomarCredencialSalon1, actionPickCredencial,
+                null, EntregaConstants.tomadoCredencial);
+
+        moveIrAPasillo = moveWithActionsAndRules(EntregaConstants.moveIrA, actionChangeToPasillo,
+                null, EntregaConstants.cambiadoAPasillo);
+        moveIrASalon1 = moveWithActionsAndRules(EntregaConstants.moveIrA, actionChangeToSalon1,
+                null, EntregaConstants.cambiadoASalon1);
+        moveIrASalon2 = moveWithActionsAndRules(EntregaConstants.moveIrA, actionChangeToSalon2,
+                null, EntregaConstants.cambiadoASalon2);
+        moveIrASalon3 = moveWithActionsAndRules(EntregaConstants.moveIrA, actionChangeToSalon3,
+                null, EntregaConstants.cambiadoASalon3);
+        moveIrAAccesoBiblioteca = moveWithActionsAndRules(EntregaConstants.moveIrA, actionChangeToAccesoBiblioteca,
+                null, EntregaConstants.cambiadoAAccesoBiblioteca);
+
+        movePonerFotoEnCredencial = moveWithActionsAndRules(EntregaConstants.movePutFoto, actionPutFotoEnCredencial,
+                null, EntregaConstants.cambiadoFotoDeCredencial);
+        movePonerFotoEnCredencial.addAction(actionSetCredencialToValida);
+    }
+
+    private void addMoves() {
+        itemCuadroBarco.addMove(moveMoverCuadro);
+        itemCajaFuerte.addMove(moveAbrirCajaFuerte);
+        itemCredencial.addMove(moveTomarCredencial);
+
+        doorAccesoBibliotecaToPasillo.addMove(moveIrAPasillo);
+        doorSalon1ToPasillo.addMove(moveIrAPasillo);
+        doorSalon2ToPasillo.addMove(moveIrAPasillo);
+        doorSalon3ToPasillo.addMove(moveIrAPasillo);
+
+        doorPasilloToAccesoBiblioteca.addMove(moveIrAAccesoBiblioteca);
+        doorBibliotecaToAccesoBiblioteca.addMove(moveIrAAccesoBiblioteca);
+        doorPasilloToSalon1.addMove(moveIrASalon1);
+        doorPasilloToSalon2.addMove(moveIrASalon2);
+        doorPasilloToSalon3.addMove(moveIrASalon3);
+
+        itemCredencial.addMove(movePonerFotoEnCredencial);
+    }
+
+    private void createItems() {
+        // Items from character
+        itemFoto = createAndAddElement(EntregaConstants.photo, character,null);
+        itemLapicera = createAndAddElement(EntregaConstants.pen, character,null);
+
+        createItemsSalon1();
+        createItemsSalon2();
+        createItemsSalon3();
+        createItemsAccesoBiblioteca();
+    }
+
+    private void createItemsSalon1() {
+        itemMesa = createAndAddElement(EntregaConstants.tableSalon1, roomSalon1,null);
+        itemBotella = createAndAddElement(EntregaConstants.bottleSalon1, roomSalon1,null);
+        itemVaso1 = createAndAddElement(EntregaConstants.glass1Salon1, roomSalon1,null);
+        itemVaso2 = createAndAddElement(EntregaConstants.glass2Salon1, roomSalon1,null);
+        itemSilla1 = createAndAddElement(EntregaConstants.chair1Salon1, roomSalon1,null);
+        itemSilla2 = createAndAddElement(EntregaConstants.chair2Salon1, roomSalon1,null);
+        itemCuadroTren = createAndAddElement(EntregaConstants.cuadroTrenSalon1, roomSalon1,null);
+
+        itemCuadroBarco = createAndAddElement(EntregaConstants.cuadroBarcoSalon1, roomSalon1,null);
+        itemCajaFuerte = createAndAddElement(EntregaConstants.cajaFuerteSalon1, itemCuadroBarco,null);
+        itemCredencial = createAndAddElement(EntregaConstants.credencialSalon1, itemCajaFuerte,null);
+    }
+
+    private void createItemsSalon2() {
+        itemMartillo = createAndAddElement(EntregaConstants.martilloSalon2, roomSalon2,null);
+        itemDestornillador1 = createAndAddElement(EntregaConstants.destornillador1Salon2, roomSalon2,null);
+        itemDestornillador2 = createAndAddElement(EntregaConstants.destornillador2Salon2, roomSalon2,null);
+    }
+
+    private void createItemsSalon3() {
+        itemLlave = createAndAddElement(EntregaConstants.keySalon3, roomSalon3,null);
+    }
+
+    private void createItemsAccesoBiblioteca() {
+        itemBibliotecario = createAndAddElement(EntregaConstants.bibliotecario, roomAccesoBiblioteca,null);
+    }
 }
-
-//public final class EntregaBuilder extends GameBuilderImp {
-//
-//}
--- EntregaBuilder/src/main/java/EntregaConstants.java
+++ EntregaBuilder/src/main/java/EntregaConstants.java
@@ -2,16 +2,68 @@
 public class EntregaConstants {
     //Elements
     public static final String character = "character";
-    public static final String room = "room";
-    public static final String stick = "stick";
+    public static final String roomPasillo = "pasillo";
+    public static final String roomSalon1 = "salon1";
+    public static final String roomSalon2 = "salon2";
+    public static final String roomSalon3 = "salon3";
+    public static final String roomBiblioteca = "biblioteca";
+    public static final String roomAccesoBiblioteca = "accesoBiblioteca";
+    public static final String roomSotano = "sotano";
+    public static final String roomSubSotano = "subsotano";
+    public static final String roomPatio = "patio";
+
+    public static final String doorPasilloToSalon1 = "doorSalon1";
+    public static final String doorPasilloToSalon2 = "doorSalon2";
+    public static final String doorPasilloToSalon3 = "doorSalon3";
+    public static final String doorAccesoBiblioteca = "doorAccesoBiblioteca";
+    public static final String doorPasillo = "doorPasillo";
+    public static final String doorBiblioteca = "doorBiblioteca";
+
+    //Items
+    public static final String pen = "lapicera";
+    public static final String photo = "foto";
+    public static final String tableSalon1 = "mesa";
+    public static final String bottleSalon1 = "botella";
+    public static final String glass1Salon1 = "vasoRojo";
+    public static final String glass2Salon1 = "vasoAzul";
+    public static final String chair1Salon1 = "sillaRoja";
+    public static final String chair2Salon1 = "sillaAzul";
+    public static final String cuadroBarcoSalon1 = "cuadroBarco";
+    public static final String cajaFuerteSalon1 = "cajaFuerte";
+    public static final String credencialSalon1 = "credencial";
+    public static final String cuadroTrenSalon1 = "cuadroTren";
+    public static final String martilloSalon2 = "martillo";
+    public static final String destornillador1Salon2 = "destornilladorRojo";
+    public static final String destornillador2Salon2 = "destornilladorAzul";
+    public static final String keySalon3 = "llave";
+    public static final String bibliotecario = "bibliotecario";
+
 
     //Actions
-    public static final String pick = "pick";
+    public static final String abierta = "abierta";
+    public static final String valido = "valida";
+    public static final String invalido = "invalida";
+    public static final String feliz = "feliz";
 
     //Rules
-    public static final String noStick = "There's no stick in room";
-    public static final String notWon = "it's a pity";
+    public static final String necesitaTenerLlaveSalon3 = "Necesitas una llave para eso!";
+    public static final String necesitaSerValida = "Parece que eso no es valido...";
 
     //Moves
-    public static final String pickStick = "You won the game!";
+    public static final String tomarCredencialSalon1 = "Has obtenido una credencial";
+    public static final String cambiadoAPasillo = "Es un pasillo";
+    public static final String cambiadoASalon1 = "Es un salon";
+    public static final String cambiadoASalon2 = "Es un salon";
+    public static final String cambiadoASalon3 = "Es un salon";
+    public static final String cambiadoAAccesoBiblioteca = "Es el acceso a una biblioteca";
+    public static final String cambiadoFotoDeCredencial = "Ahora la credencial parece tuya!";
+
+    public static final String movedCuadroBarco = "Hay algo atras del cuadro...";
+    public static final String abiertaCajaFuerte = "Parece que hay algo en la caja fuerte!";
+    public static final String tomadoCredencial = "Ahora tienes una credencial.. de donde puede ser?";
+
+    public static final String moveIrA = "go to";
+    public static final String movePutFoto = "put foto";
+    public static final String moveCuadro = "move";
+    public static final String moveAbrirCajaFuerte = "open";
 }
