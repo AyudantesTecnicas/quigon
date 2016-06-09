c7d2b0253095ec6f42bc29da8af978f571f8774f
meister4ever <sasavulsky@gmail.com>
2016-06-09 14:43:33 -0300

    Cambio de 2 a 4 jugadores

--- EntregaBuilder/src/main/java/EntregaConstants.java
+++ EntregaBuilder/src/main/java/EntregaConstants.java
@@ -11,7 +11,7 @@ public class EntregaConstants {
     public static final String elementoVacio = "";
 
     //Elements
-    public static final int numberOfPlayers = 2;
+    public static final int numberOfPlayers = 4;
     public static final String roomPasillo = "pasillo";
     public static final String roomSalon1 = "salon1";
     public static final String roomSalon2 = "salon2";
